4d3b60dc552a7f0fa807ecdacc98c7ec27881348
Abdul Husain <abdulhusaindahodwala@gmail.com>
2026-01-19 15:46:38 +0530

    Vercel deployed

--- ExpenseTracker_Backend/server.js
+++ ExpenseTracker_Backend/server.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://money-track-frontend-sigma.vercel.app"], // Add your Vercel link here
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
